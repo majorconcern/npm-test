d94076856b4c34b090b45d1d94a2e3128bb583c5
lang <jinren.lang@gmail.com>
2023-09-20 17:02:01 +0800

    package.json

--- layout/packages/icon-svg/index.vue
+++ layout/packages/icon-svg/index.vue
@@ -46,11 +46,11 @@ export default {
 </script>
 
 <style>
-.icon-svg {
+/* .icon-svg {
 	width: 1em;
 	height: 1em;
 	vertical-align: -0.15em;
 	fill: currentColor;
 	overflow: hidden;
-}
+} */
 </style>
